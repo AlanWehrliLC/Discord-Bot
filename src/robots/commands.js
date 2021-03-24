8916ee6c665641ae0e99be5e1d9baf363418b96c
Alan Wehrli <64156830+AlanWehrliLC@users.noreply.github.com>
2021-03-23 23:02:25 -0300

    Update commands.js

--- src/robots/commands.js
+++ src/robots/commands.js
@@ -34,7 +34,7 @@ function robot(db, client){
     if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
     if(!message.content === '!commands') return message.reply('Use o comando "!helpRemoveCommand", para saber como usar o comando "!removeCommand"')
-    if (message.member.hasPermission('ADMINISTRATOR') && message.content === "!commands") {
+    if (message.content === "!commands") {
       const docRef = db.collection(client.user.id).doc(message.guild.id)
       docRef.get().then((doc)=>{
         const commands = doc.data()
